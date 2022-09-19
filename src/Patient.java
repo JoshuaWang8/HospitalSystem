f06296c82dc9ef1fd5dad7a6dd0a394500c693d2
Joshua Wang <wangjoshua8@gmail.com>
2022-09-19 22:22:19 +1000

    Update compareTo method for Patients.

--- src/Patient.java
+++ src/Patient.java
@@ -6,8 +6,27 @@ public class Patient extends PatientBase {
 
     @Override
     public int compareTo(PatientBase o) {
-        /* Add your code here! */
-        return 0;
+        String timeParts[] = this.getTime().split(":");
+        int hour1 = Integer.parseInt(timeParts[0]);
+        int minutes1 = Integer.parseInt(timeParts[1]);
+
+        timeParts = o.getTime().split(":");
+        int hour2 = Integer.parseInt(timeParts[0]);
+        int minutes2 = Integer.parseInt(timeParts[1]);
+
+        if (hour1 < hour2) {
+            return -1;
+        } else if (hour1 > hour2) {
+            return 1;
+        } else {
+            if (minutes1 < minutes2) {
+                return -1;
+            } else if (minutes1 > minutes2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 
     /* Add any extra functions below */
