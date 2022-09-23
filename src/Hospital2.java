46f461984572dbb075b3222a8ef0cc7e9ef4ea47
Joshua Wang <wangjoshua8@gmail.com>
2022-09-23 13:00:12 +1000

    Fix bug where patients are added in incorrect order into linked list. Add checks to ensure patients request valid appointment time.

--- src/Hospital2.java
+++ src/Hospital2.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 public class Hospital2 extends HospitalBase {
 
     /** Pointer to the head of the linked list of appointments. */
-    Node head;
+    private Node head;
 
     /** Node for the linked list. */
     class Node {
@@ -22,18 +22,33 @@ public class Hospital2 extends HospitalBase {
 
     @Override
     public boolean addPatient(PatientBase patient) {
+        // Get the scheduled time into integers (hour and minutes separated)
+        String timeParts[] = patient.getTime().split(":");
+        int hour = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+
+        // Check times are valid (correct format, during work hours and not during lunch times)
+        if ((hour < 8) || (hour > 17) || (hour == 12) || (minutes < 0) || (minutes > 59)) {
+            return false;
+        }
+
         Node newPatient = new Node((Patient) patient);
 
         // Add new node to correct position in linked list
-        if (head == null) {
-            head = newPatient;
+        if ((head == null) || (head.patient.compareTo(newPatient.patient) > 0)) {
+            newPatient.next = this.head;
+            this.head = newPatient;
         } else {
             Node current = this.head;
-            while ((current.next != null) && (current.patient.compareTo(newPatient.patient) < 0)) {
+
+            while ((current.next != null) && (newPatient.patient.compareTo(current.next.patient) >= 0)) {
                 current = current.next;
             }
+
+            newPatient.next = current.next;
             current.next = newPatient;
         }
+
         return true;
     }
 
